bada6e3f56c4d36303bf3e3cc696aea56a954b4e
liujie <43362827+liujie940228@users.noreply.github.com>
2022-10-27 10:22:29 +0800

    Update random.f90
    new version of compiler don't recognizer this sentences any more, so just try to remove these.

--- src/random.f90
+++ src/random.f90
@@ -52,124 +52,124 @@
 ! changed for use in f90
 ! -------------------------------------------------------------------
 
-module RNG_RLFSR113
-  use MyNumbers
-  implicit none
+!module RNG_RLFSR113
+! use MyNumbers
+! implicit none
   
   ! accessibility
-  private
-  public :: rlfsr113
-  public :: lfsrinit
+!  private
+!  public :: rlfsr113
+!  public :: lfsrinit
   
   ! variables
-  integer(kind=ikind) :: z1, z2, z3, z4
+!  integer(kind=ikind) :: z1, z2, z3, z4
  
   ! parameter
-  real(kind=rkind),    parameter :: AM = 4.656612873077d-10
-  integer(kind=ikind), parameter :: IA = 16807
-  integer(kind=ikind), parameter :: IM = 2147483647
-  integer(kind=ikind), parameter :: IQ = 127773
-  integer(kind=ikind), parameter :: IR = 2836
+!  real(kind=rkind),    parameter :: AM = 4.656612873077d-10
+!  integer(kind=ikind), parameter :: IA = 16807
+!  integer(kind=ikind), parameter :: IM = 2147483647
+!  integer(kind=ikind), parameter :: IQ = 127773
+!  integer(kind=ikind), parameter :: IR = 2836
   
-contains
+!contains
 
   ! -----------------------------------------------------------------
   ! rlfsr113()  returns a random number of interval (0, 1)
   ! -----------------------------------------------------------------
-  function rlfsr113() result(dRet)
-    real(kind=rkind)        :: dRet
+!  function rlfsr113() result(dRet)
+!    real(kind=rkind)        :: dRet
 
-    integer(kind=ikind) :: b
+!    integer(kind=ikind) :: b
 
-    b  = ishft(ieor(ishft(z1,6),z1),-13)
-    z1 = ieor(ishft(iand(z1,-2),18),b)
+!    b  = ishft(ieor(ishft(z1,6),z1),-13)
+!   z1 = ieor(ishft(iand(z1,-2),18),b)
 
-    b  = ishft(ieor(ishft(z2,2),z2),-27)
-    z2 = ieor(ishft(iand(z2,-8),2),b)
+!    b  = ishft(ieor(ishft(z2,2),z2),-27)
+!    z2 = ieor(ishft(iand(z2,-8),2),b)
 
-    b  = ishft(ieor(ishft(z3,13),z3),-21)
-    z3 = ieor(ishft(iand(z3,-16),7),b)
+!    b  = ishft(ieor(ishft(z3,13),z3),-21)
+!    z3 = ieor(ishft(iand(z3,-16),7),b)
 
-    b  = ishft(ieor(ishft(z4,3),z4),-12)
-    z4 = ieor(ishft(iand(z4,-128),13),b)
+!    b  = ishft(ieor(ishft(z4,3),z4),-12)
+!    z4 = ieor(ishft(iand(z4,-128),13),b)
 
-    dRet=ishft(ieor(ieor(ieor(z1,z2),z3),z4),-1)*AM
+!   dRet=ishft(ieor(ieor(ieor(z1,z2),z3),z4),-1)*AM
 
-  end function rlfsr113
+!  end function rlfsr113
 
   ! -----------------------------------------------------------------
   ! lfsrinit()  initialize rlfsr113 (z1,z2,z3,z4) 
   ! -----------------------------------------------------------------
-  subroutine lfsrinit(idum)
-    integer(kind=ikind), intent(inout) :: idum
+!  subroutine lfsrinit(idum)
+!    integer(kind=ikind), intent(inout) :: idum
 
-    integer(kind=ikind) :: k,c1,c2,c3,c4
-    real(kind=rkind)    :: dummy
+!    integer(kind=ikind) :: k,c1,c2,c3,c4
+!    real(kind=rkind)    :: dummy
     
 
     ! Check whether the FORTRAN integers can be used as unsigned long !
 
     ! data c1 /B'11111111111111111111111111111110'/
     !  data c1 /X'FFFFFFFE'/
-    c1 = Z"FFFFFFFE"
+!    c1 = Z"FFFFFFFE"
     ! data c2 /B'11111111111111111111111111111000'/
     !  data c2 /X'FFFFFFF8'/
-    c2 = Z"FFFFFFF8"
+!    c2 = Z"FFFFFFF8"
     ! data c3 /B'11111111111111111111111111110000'/
     !  data c3 /X'FFFFFFF0'/
-    c3 = Z"FFFFFFF0"
+!    c3 = Z"FFFFFFF0"
     ! data c4 /B'11111111111111111111111110000000'/
     !  data c4 /X'FFFFFF80'/
-    c4 = Z"FFFFFF80"
+!    c4 = Z"FFFFFF80"
     
-    if ((c1.ne.-2).or.(c2.ne.-8).or.(c3.ne.-16).or.(c4.ne.-128)) then
-      print *,"c1,c2,c3,c4", c1,c2,c3,c4
-      print *,'Nonstandard integer representation. Stoped.'
-      stop
-    endif
+!    if ((c1.ne.-2).or.(c2.ne.-8).or.(c3.ne.-16).or.(c4.ne.-128)) then
+!      print *,"c1,c2,c3,c4", c1,c2,c3,c4
+!      print *,'Nonstandard integer representation. Stoped.'
+!      stop
+!    endif
 
     ! Initialize z1,z2,z3,z4
 
-    if (idum.le.0) idum=1
-    k=(idum)/IQ
-    idum=IA*(idum-k*IQ)-IR*k
-    if (idum.lt.0) idum = idum + IM
-    if (idum.lt.2) then
-      z1=idum+2 
-    else 
-      z1=idum
-    endif
-    k=(idum)/IQ
-    idum=IA*(idum-k*IQ)-IR*k
-    if (idum.lt.0) idum = idum + IM
-    if (idum.lt.8) then 
-      z2=idum+8 
-    else 
-      z2=idum
-    endif
-    k=(idum)/IQ
-    idum=IA*(idum-k*IQ)-IR*k
-    if (idum.lt.0) idum = idum + IM
-    if (idum.lt.16) then
-      z3=idum+16 
-    else 
-      z3=idum
-    endif
-    k=(idum)/IQ
-    idum=IA*(idum-k*IQ)-IR*k
-    if (idum.lt.0) idum = idum + IM
-    if (idum.lt.128) then
-      z4=idum+128 
-    else 
-      z4=idum
-    endif
+!    if (idum.le.0) idum=1
+!    k=(idum)/IQ
+!    idum=IA*(idum-k*IQ)-IR*k
+!    if (idum.lt.0) idum = idum + IM
+!    if (idum.lt.2) then
+!      z1=idum+2 
+!    else 
+!      z1=idum
+!    endif
+!    k=(idum)/IQ
+!    idum=IA*(idum-k*IQ)-IR*k
+!    if (idum.lt.0) idum = idum + IM
+!    if (idum.lt.8) then 
+!      z2=idum+8 
+!    else 
+!      z2=idum
+!    endif
+!    k=(idum)/IQ
+!    idum=IA*(idum-k*IQ)-IR*k
+!    if (idum.lt.0) idum = idum + IM
+!    if (idum.lt.16) then
+!      z3=idum+16 
+!    else 
+!      z3=idum
+!    endif
+!    k=(idum)/IQ
+!    idum=IA*(idum-k*IQ)-IR*k
+!    if (idum.lt.0) idum = idum + IM
+!    if (idum.lt.128) then
+!      z4=idum+128 
+!    else 
+!      z4=idum
+!    endif
 
     ! Make a single call to rlfsr113() to achieve a valid state
-    dummy=rlfsr113()
+!    dummy=rlfsr113()
       
-  end subroutine lfsrinit
+!  end subroutine lfsrinit
 
-end module RNG_RLFSR113
+!end module RNG_RLFSR113
 
 ! -------------------------------------------------------------------
 ! 
